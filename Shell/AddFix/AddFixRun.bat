8be17974a5891a537aad64684a0edf08e064eeb1
Kangrok Kim <kimpro@uebermensch.tech>
2023-10-31 03:36:38 +0900

    Update AddFixRun.bat

--- Shell/AddFix/AddFixRun.bat
+++ Shell/AddFix/AddFixRun.bat
@@ -1,6 +1,6 @@
 @REM Run AddFix.vbs
 @REM 2023.10.30
 
-cscript AddFix.vbs txt prefix pf_
+cscript AddFix.vbs .txt prefix pf_ test
 @REM cscript AddFix.vbs .txt suffix _sf
-cscript AddFix.vbs test replace pf_ " "
+cscript AddFix.vbs .txt replace pf_ " " test
