1c87b22ecbc93687d5a2009e0ab220eb063c2567
Kangrok Kim <kimpro@uebermensch.tech>
2024-02-16 01:14:16 +0900

    Create Lotto.r

---
+++ R/Lotto/Lotto.r
@@ -0,0 +1,27 @@
+# Lottery Number Generator
+# 2024.02.15
+
+# This script generates Lotto numbers by randomly selecting 6 unique numbers 
+# between 1 and 45. It repeats the process 20 times and prints the selected numbers.
+
+BALLS = 45
+CHOOSE = 6
+PURCHASE = 20
+
+for (i in 1:PURCHASE) {
+    while (TRUE) {
+        # Generate 6 random numbers between 1 and 42
+        nums <- c(sort(ceiling(runif(CHOOSE) * BALLS)))
+        luckyNum <- ceiling(runif(1) * BALLS)
+        # Check if there are exactly 6 unique numbers and luckyNum is not in nums
+        if (!any(duplicated(nums)) && !any(luckyNum %in% nums)) {
+            break;
+        }
+    }
+
+    # Print the selected numbers with fixed width
+    for (j in 1:CHOOSE) {
+        cat(sprintf("%2d ", nums[j]))
+    }
+    cat("/", sprintf("%2d", luckyNum), "\n")
+}
